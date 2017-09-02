a6be80329a5b89ffc7e894c51cfee87692660a2a
SunooBertsch <sbertsch92@yahoo.com>
2017-09-02 16:37:02 -0700

    added webview, non-specific url

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,35 +1,21 @@
-import React from 'react';
-import { Text, TextInput, View } from 'react-native'; //View is primitve element to position/wrap/style
-import SearchBar from './searchbar.js';
+import React from "react";
+import { Text, TextInput, View } from "react-native"; //View is primitve element to position/wrap/style
+import SearchBar from "./searchbar.js";
 
-
-const Landing = (props) => {
-	const { textStyle, textBar, viewStyle } = styles;
-
-	return (
-	<View style ={viewStyle}> 
-		<SearchBar />
-	</View>	
-	);
+const Landing = props => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        height: 700,
+        padding: 20,
+        alignSelf: "stretch"
+      }}>
+      <SearchBar />
+    </View>
+  );
 };
 
 //camel case instead of dashes for object css
-const styles = {
-	viewStyle: {
-		backgroundColor: '#F8F8F8',
-		justifyContent: 'center',
-		alignItems: 'center',
-		height: 60,
-		paddingTop: 15,
-		shadowColor: '#000',
-		shadowOffset: { width: 0, height: 2 },
-		shadowOpacity: 0.2,
-		elevation: 2,
-		position: 'relative'
-	},
-	textStyle: {
-		fontSize: 20
-	},
-};
 
 export default Landing;
