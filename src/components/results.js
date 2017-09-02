39773f663439e150992752512d563c78943fbb18
SunooBertsch <sbertsch92@yahoo.com>
2017-09-02 15:39:16 -0700

    generating genre specific movie title

--- src/components/results.js
+++ src/components/results.js
@@ -6,24 +6,30 @@ class Result extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: this.props.query
+      result: ""
     };
   }
 
   componentWillMount() {
+    let title = "";
     fetch(
-      `https://api.themoviedb.org/3/genre/28/movies?api_key=b09e8ebef5593dfec03034ec1ab31d35&language=en-US&include_adult=false&sort_by=created_at.desc`
+      `https://api.themoviedb.org/3/genre/${this.props
+        .query}/movies?api_key=b09e8ebef5593dfec03034ec1ab31d35&language=en-US&include_adult=false&sort_by=created_at.desc`
     )
       .then(resp => {
-        console.log("worked!");
-        console.log(resp.json());
+        return resp.json();
       })
-      .catch(resp => {
-        console.log("Error");
+      .then(json => {
+        title = json.results[Math.floor(Math.random() * 20) + 1].title;
+        console.log(title);
       });
   }
   render() {
-    return <Text>Hey</Text>;
+    return (
+      <Text>
+        ${this.state.result}
+      </Text>
+    );
   }
 }
 
--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -34,7 +34,44 @@ class SearchBar extends Component {
         </View>
       );
     } else {
-      return <Result query={this.state.text} />;
+      let genre = this.state.text;
+      let id = "";
+      switch (genre) {
+        case "Action":
+          id = "28";
+          break;
+        case "Comedy":
+          id = "35";
+          break;
+        case "Horror":
+          id = 27;
+          break;
+        case "Thriller":
+          id = "53";
+          break;
+        case "Sci-Fi":
+          id = "878";
+          break;
+        case "Animation":
+          id = "16";
+          break;
+        case "Western":
+          id = "37";
+          break;
+        case "Family":
+          id = "10751";
+          break;
+        case "Mystery":
+          id = "9648";
+          break;
+        case "Romance":
+          id = "10749";
+          break;
+        case "Drama":
+          id = "18";
+          break;
+      }
+      return <Result query={id} />;
     }
   }
 }
